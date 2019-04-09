b43211bf5e63c40e4b59de4e088b20613b410d7d
George Bougakov <githubinq@bygeorgenet.me>
2019-04-09 19:42:01 +0300

    Change project directory

--- server/index.js
+++ server/index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.use(express.static('assets'))
+app.use(express.static('../dist'))
 app.use(express.static('/var/www/html'))
 
 app.use((req, res) => {
-  res.sendFile(path.join(__dirname, 'assets', 'index.html'))
+  res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'))
 })
 
 app.listen(5056)
