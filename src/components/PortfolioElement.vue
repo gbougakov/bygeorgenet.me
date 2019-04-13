7c338e8c21b3e68e3d999e3b90730af054b01074
George Bougakov <githubinq@bygeorgenet.me>
2019-04-13 19:21:39 +0300

    What was I thinking? This is Vue! You can template anything!

---
+++ src/components/PortfolioElement.vue
@@ -0,0 +1,27 @@
+<template>
+  <a v-bind:href="url" class="uk-link-reset">
+    <div class="uk-card uk-card-default uk-card-small uk-card-body uk-margin uk-card-hover">
+      <div class="uk-grid-small uk-flex uk-flex-middle" uk-grid>
+        <div class="uk-width-1-5 uk-padding-small uk-padding-remove-vertical">
+          <img v-bind:src="image">
+        </div>
+        <div class="uk-width-expand">
+          <h4 class="uk-text-bold uk-margin-remove">{{title}}</h4>
+          <p class="uk-margin-remove">{{subtitle}}</p>
+          <p class="uk-margin-remove uk-text-small">{{date}}</p>
+        </div>
+      </div>
+    </div>
+  </a>
+</template>
+
+<script>
+  export default {
+    name: 'portfolio-element',
+    props: ['image', 'title', 'subtitle', 'date', 'url'],
+    data: function () {
+      return {}
+    }
+  }
+
+</script>
--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,46 +1,36 @@
 <template>
   <div>
     <h1 class="uk-text-bold">2019</h1>
-    <div class="uk-card uk-card-default uk-card-small uk-card-body uk-margin">
-      <div class="uk-grid-small uk-flex uk-flex-middle" uk-grid>
-        <div class="uk-width-1-5 uk-padding-small uk-padding-remove-vertical">
-          <img src="@/assets/skyeng.png">
-        </div>
-        <div class="uk-width-expand">
-          <h4 class="uk-text-bold uk-margin-remove">Skyeng - largest Russian edtech startup</h4>
-          <p class="uk-margin-remove">Currently work there as a paid intern</p>
-          <p class="uk-margin-remove uk-text-small">January 30th, 2019 - Present</p>
-        </div>
-      </div>
-    </div>
+    <portfolio-element image="img/producthunt.png" title="Product Hunt"
+      subtitle="Docket featured as Product of the Day (#1)" date="April 13th, 2019" url="https://www.producthunt.com/posts/docket-3">
+    </portfolio-element>
+    <portfolio-element image="img/skyeng.png" title="Skyeng - largest Russian edtech startup"
+      subtitle="Currently work there as a paid intern" date="January 30th, 2019 - Present" url="https://en.skyeng.ru">
+    </portfolio-element>
+
     <h1 class="uk-text-bold">2018</h1>
-    <div class="uk-card uk-card-default uk-card-small uk-card-body uk-margin">
-      <div class="uk-grid-small uk-flex uk-flex-middle" uk-grid>
-        <div class="uk-width-1-5 uk-padding-small uk-padding-remove-vertical">
-          <img src="@/assets/makerfaire.png">
-        </div>
-        <div class="uk-width-expand">
-          <h4 class="uk-text-bold uk-margin-remove">Moscow Maker Faire</h4>
-          <p class="uk-margin-remove">Participant/Maker</p>
-          <p class="uk-margin-remove uk-text-small">September 8-9th, 2018</p>
-        </div>
-      </div>
-    </div>
-    <div class="uk-card uk-card-default uk-card-small uk-card-body uk-margin">
-      <div class="uk-grid-small uk-flex uk-flex-middle" uk-grid>
-        <div class="uk-width-1-5 uk-padding-small uk-padding-remove-vertical">
-          <img src="@/assets/producthunt.png">
-        </div>
-        <div class="uk-width-expand">
-          <h4 class="uk-text-bold uk-margin-remove">Product Hunt</h4>
-          <p class="uk-margin-remove">Featured as Product of the Day #3</p>
-          <p class="uk-margin-remove uk-text-small">August 18th, 2018</p>
-        </div>
-      </div>
-    </div>
+    <portfolio-element image="img/makerfaire.png" title="Moscow Maker Faire"
+      subtitle="Participant/Maker" date="September 8-9th, 2018" url="https://moscow.makerfaire.com">
+    </portfolio-element>
+    <portfolio-element image="img/producthunt.png" title="Product Hunt"
+      subtitle="Que featured as Product of the Day (#3)" date="August 18th, 2018" url="https://www.producthunt.com/posts/que">
+    </portfolio-element>
   </div>
 </template>
 
 <style scoped>
 
 </style>
+
+<script>
+  // @ is an alias to /src
+  import PortfolioElement from '@/components/PortfolioElement.vue'
+
+  export default {
+    name: 'Projects',
+    components: {
+      PortfolioElement
+    }
+  }
+
+</script>
