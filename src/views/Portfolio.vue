197e2a98f66bcc5c958bf2afe01c3960f45bcf82
George Bougakov <githubinq@bygeorgenet.me>
2019-04-15 16:11:09 +0300

    damn I was close!

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -2,7 +2,7 @@
   <div>
     <h1 class="uk-text-bold">2019</h1>
     <portfolio-element image="img/producthunt.png" title="Product Hunt"
-      subtitle="Docket featured as Product of the Day (#1)" date="April 13th, 2019" url="https://www.producthunt.com/posts/docket-3">
+      subtitle="Docket featured as Product of the Day (#2)" date="April 13th, 2019" url="https://www.producthunt.com/posts/docket-3">
     </portfolio-element>
     <portfolio-element image="img/skyeng.png" title="Skyeng - largest Russian edtech startup"
       subtitle="Currently work there as a paid intern" date="January 30th, 2019 - Present" url="https://en.skyeng.ru">
