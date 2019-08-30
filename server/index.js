82f6a80007081b08442a88ba8619bde5d8707f42
George Bougakov <githubinq@bygeorgenet.me>
2019-08-30 16:26:36 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,11 +2,18 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+// Redirect to my new website
+app.use((req, res) => {
+  res.redirect('https://gbougakov.dev')
+})
+
+/*
 app.use(express.static('../dist'))
 app.use(express.static('/var/www/html'))
 
 app.use((req, res) => {
   res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'))
 })
+*/
 
 app.listen(5056)
